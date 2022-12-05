49a75b61bf37c6430cc09c62d600c23a2fa24539
constlo <kaltsu1216@gmail.com>
2022-12-05 12:12:30 +0200

    Datalla toteutus

--- kmeans_final.py
+++ kmeans_final.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from itertools import chain
 import mysql.connector
 import pandas as pd
 
@@ -18,55 +17,26 @@ mycursor = mydb.cursor()
 mycursor.execute("SELECT * FROM rawdata WHERE groupId=66")
 
 myresult = pd.DataFrame(mycursor.fetchall(), columns=['id', 'dateTime', 'groupId', 'a', 'b', 'x', 'y', 'z', 'c', 'flag', 'status'])
+
 #Otetaan talteen x-y-ja z-arvot. Nämä sijoitetaan data2DArray-tietorakenteeseen.
 databaseX = myresult['x'].to_numpy(dtype=int)
 databaseY = myresult['y'].to_numpy(dtype=int)
 databaseZ = myresult['z'].to_numpy(dtype=int)
 
+#Emme voi tietää, ovatko kaikki akselit yhtä pitkiä. Siispä selvitetään suurin pituus np.max-funktiolla.
 resultSize = np.max([databaseX.size, databaseY.size, databaseZ.size])
 
 data2DArray = np.zeros(resultSize * 3).reshape(resultSize, 3)
 
+#sijoita tietorakenteeseen
 data2DArray[:, 0] = databaseX
 data2DArray[:, 1] = databaseY
 data2DArray[:, 2] = databaseZ
 print(data2DArray)
 pd.DataFrame.to_csv(myresult, 'data.csv')
 
-
-
-#lue putty.log tiedosto ja muokkaa se numpy arrayksi
-#Käytetään squeeze-metodia, jolla isnompiulotteinen array muokataan peinemmäksi
-mydf = np.asarray(pd.read_csv('putty.log').to_numpy().squeeze(1))
-numberOfRows = 0
-arraySize = mydf.size
-threeDivisible = mydf.size % 3
-if(threeDivisible != 0):
-    #jos arrayn koko ei ole kolmella jaollinen, täytyy loput alkiot leikata pois.
-    #Tässä tapauksessa 119 - 2 = 117 -> jaollinen kolmella
-    arraySize -= threeDivisible
-#Leikataan 2 viimeistä alkiota pois.
-handledArray = np.copy(mydf[0:arraySize])
-lastIndex = int(arraySize / 3)
-#Muokataan vielä jonosta 2d-array
-twoDarray = handledArray.reshape(int(handledArray.size / 3), 3)
-
-#Viikko 5, vaihe 2: Keskipisteiden arpominen
-"""
-Määritellään algoritmissa tarvittavia datarakenteita (keskipisteet):
-Keskipisteet, joita on 4 kpl ja kullakin keskipisteellä on x,y,z komponentti. 
-Tämä data voidaan siis pitää esim 4 riviä, 3 saraketta käsittävässä numpy matriisissa.
-Arvotaan nämä 4 keskipistettä numpyn random.rand() funktion avulla siten, 
-että arvottujen satunnaisten lukujen arvot ovat 0 ja suurimman x,y,z arvon välillä. 
-Eli satunnaiset arvot skaalataan input datan mukaan.
-
-"""
-#luodaan x, y, ja z-arvoparit
-x_ax = twoDarray[0:40, 0]
-y_ax = twoDarray[0:40, 1]
-z_ax = twoDarray[0:40, 2]
-#luodaan myös lista, jossa säilytetään akselien maksimiarvot
-maxVals = [x_ax.max(), y_ax.max(), z_ax.max()]
+#Varastoi maksimiarvot
+maxVals = [databaseX.max(), databaseY.max(), databaseZ.max()]
 
 #Tämän jälkeen luodaan (4x3) matriisi, jossa säilytetään 4 satunnaista pistettä.
 keskipisteet = np.zeros(12, dtype=int).reshape((4, 3))
@@ -75,38 +45,9 @@ for i in keskipisteet:
     for j in range(0,3):
         i[j] = np.random.randint(0, maxVals[j])
 
-
-"""
-Määritellään algoritmissa tarvittavia datarakenteita 
-centerPointCumulativeSum tulee olla keskipisteiden tapaan 4 riviä, 
-3 saraketta kokoinen numpy matriisi. 
-Tähän summataan aina voittavalle keskipisteelle yhden datapisteen x,y,z komponentit
-Counts tulee olla 1 riviä 4 saraketta kokoinen numpy matriisi
-ja tänne kasvatetaan aina voittavan keskipisteen datapisteiden lukumäärää yhdellä jokaisen voiton jälkeen.
-Distances on myös 1 riviä 4 saraketta kokoinen numpy matriisi 
-ja tähän talletetaan laskennan edetessä yksittäisen x,y,z pisteen etäisyys kaikkiin
-keskipisteet datarakenteessa oleviin 4 keskipisteeseen ja nuo 4 etäisyysarvoa talletetaan tähän muuttujaan.
-"""
-
-
+#Luodaan distances-taulukko etäisyyksien säilyttämistä varten.
 Distances = np.zeros(4, dtype=float)
 
-"""
-Opetellaan laskemaan ns euklidinen etäisyys kahden 3D-pisteen välillä. 
-Käytä esim np.linalg.norm funktiota. 
-Ja tietysti keksit jotkut 2 kpl yksinkertaisia x,y,z pisteitä 3D-avaruudesta, 
-joiden etäisyyden varmuudella tiedät tai osaat käsin laskea.
-
-Toteutetaan kahden for luupin rakenne algoritmin ytimeksi:
-Ulompi for luuppi ”kiertää” numberOfRows kertaa 
-eli käsittelee kaikki tiedostosta löytynee datapisteet. 
-Joka luupin kierroksella lasketaan sisäkkäisen luupin avulla tämän kyseisen datapisteen etäisyydet 
-ja selvitetään tämän jälkeen minkä keskipisteen etäisyys oli pienin. 
-Ja tuon pienimmän etäisyyden keskipisteen count arvoa kasvatetaan yhdellä 
-ja  centerPointCumulativeSum muuttujaan summataan x,y,z komponettien arvot.
-Sisäkkäinen luuppi laskee yhden datapisteen etäisyyden kaikkiin 4 keskipisteeseen 
-ja tallentaa tuloksen distances muuttujaan.
-"""
 dataFromLoop = np.reshape(np.zeros(1200), [100, 4, 3])
 
 for iterations in range(100):
@@ -115,7 +56,7 @@ for iterations in range(100):
     centerPointCumulativeSum = np.zeros(12, dtype=int).reshape((4, 3))
     Counts = np.zeros(4, dtype=int)
     smallestIndex = 0
-    for piste in twoDarray:
+    for piste in data2DArray:
         Distances = np.zeros(4, dtype=float)
         #Valitaan sellainen etäisyys, joka on suurempi kuin kaikki mahdolliset etäisyydet. 
         pienin = 1000.0
@@ -201,5 +142,8 @@ axis[1, 1].plot(p4_ypoints)
 axis[1, 1].plot(p4_zpoints)
 axis[1, 1].set_title("keskipiste 4")
 
+pisteet =pd.DataFrame.from_dict(dataFromLoop[-1]) 
+
+pd.DataFrame.to_csv(pisteet, 'finalpoints.csv')
+
 plt.show()
-  
